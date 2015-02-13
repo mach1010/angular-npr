e2a8b3e64deb5559aa6eb1671e9dfdbe8817e8d0
mach1010 <jetstream1010@gmail.com>
2015-02-13 16:29:39 -0600

    chapter 2 examples of scope before deleting to continue with npr tutorial

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 var app = angular.module('myApp', []);
+
+app.run(function($rootScope) {
+  $rootScope.name = 'MACH1010';
+});
+
+app.controller('MyController', function($scope) {
+  $scope.person = {
+    name: 'mach1010'
+  };
+});
+
+app.controller('ParentController', function($scope) {
+  $scope.person = {
+    greeted: false
+  };
+});
+
+app.controller('ChildController', function($scope) {
+  $scope.sayHello = function() {
+    $scope.person.greeted = true;
+  };
+});
